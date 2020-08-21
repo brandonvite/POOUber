54c87034563e3bd2ec54dc76a08403c33a4e33c2
Brandon Vite <bramsel.vite@gmail.com>
2020-08-21 01:58:21 -0500

    Declarando método constructor en Python

--- Python/main.py
+++ Python/main.py
@@ -1,12 +1,9 @@
 from car import Car
+from account import Account
+
 if __name__ == "__main__":
     print("Hola Mundo")
-    car = Car()
-    car.license = "AMS123"
-    car.driver = "Andres Herrera"
+    
+    car = Car("AMS234", Account("Brandon Vite", "VIDB931013"))
     print(vars(car))
-
-    car2 = Car()
-    car2.license = "LJH123"
-    car2.driver = "Martha Vite"
-    print(vars(car2))
+    print(vars(car.driver))
