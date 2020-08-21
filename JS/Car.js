fafb54c0094e8f612e4df21899257d485b034978
Brandon Vite <bramsel.vite@gmail.com>
2020-08-21 01:48:26 -0500

    JavaScript OO, EcmaScript 6

--- JS/Car.js
+++ JS/Car.js
@@ -1,12 +1,14 @@
-function Car(license, driver){
-    this.id;
-    this.license = license;
-    this.driver = driver;
-    this.passenger;
-}
+class Car{
+    constructor(license, driver){
+        this.id;
+        this.license = license;
+        this.driver = driver;
+        this.passenger;
+    }
 
-Car.prototype.printDataCard = function(){
-    console.log(this.driver);
-    console.log(this.driver.name);
-    console.log(this.driver.document);
+    printDataCard(){
+        console.log(this.driver);
+        console.log(this.driver.name);
+        console.log(this.driver.document);
+    }
 }
