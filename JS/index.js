5b8f9c8255b9c73b494cd52a20ce2e93ff1acb46
Brandon Vite <bramsel.vite@gmail.com>
2020-08-22 17:26:17 -0500

    Aplicando herencia en Python y JavaScript

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AWS123", new Account("Brandon Vite", "VIDB931013"))
 car.passenger = 4;
 car.printDataCard();
+
+var uberX = new UberX("QWER123", new Account("Andrea Ferran", "ANDF34525435"), "Seat", "Leon")
+uberX.passenger = 4;
+uberX.printDataCard();
